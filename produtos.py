6ef20d43015d92f77764a1e813d99cf80c00c73e
salvabjj <sirifile03@gmail.com>
2026-02-05 16:41:35 -0300

    Criar produtos.py

---
+++ produtos.py
@@ -0,0 +1,52 @@
+produtos = {
+    "Eletrodomésticos": [
+        {
+            "nome": "Air Fryer Mondial 4L",
+            "preco": "R$ 399",
+            "link": "https://mercadolivre.com/sec/33LveAZ",
+            "loja": "Mercado Livre"
+        },
+        {
+            "nome": "Liquidificador Turbo",
+            "preco": "R$ 189",
+            "link": "https://mercadolivre.com/sec/2TupDRd",
+            "loja": "Mercado Livre"
+        },
+        {
+            "nome": "Micro-ondas Compacto",
+            "preco": "R$ 499",
+            "link": "https://amzn.to/46iUEb6",
+            "loja": "Amazon"
+        }
+    ],
+
+    "Eletrônicos": [
+        {
+            "nome": "Fone Bluetooth",
+            "preco": "R$ 129",
+            "link": "https://s.shopee.com.br/8KjpTzVMDC",
+            "loja": "Shopee"
+        },
+        {
+            "nome": "Smartwatch",
+            "preco": "R$ 199",
+            "link": "https://s.shopee.com.br/BO7l1QAut",
+            "loja": "Shopee"
+        },
+        {
+            "nome": "Teclado Mecânico",
+            "preco": "R$ 259",
+            "link": "https://mercadolivre.com/sec/115emZK",
+            "loja": "Mercado Livre"
+        }
+    ],
+
+    "Moda Masculina": [
+        {
+            "nome": "Tênis Adidas Grand Court",
+            "preco": "R$ 299",
+            "link": "https://www.netshoes.com.br/afiliado/rWODdSNWJGM",
+            "loja": "Netshoes"
+        }
+    ]
+}

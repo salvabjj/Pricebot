afc50fd4b71b4e051bab784ee48f8163f5501782
salvabjj <sirifile03@gmail.com>
2026-03-01 01:35:57 -0300

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -1,11 +1,11 @@
 import os
 from telegram import Bot
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN = os.getenv("8056783817:AAHPYbLVK8tt7uYnVIjPg77J3VIVya_YF18")
 CHAT_POST = os.getenv("-1003858741896")
 CHAT_REPORT = os.getenv("8309912019")
 
-bot = Bot(token=8056783817:AAHPYbLVK8tt7uYnVIjPg77J3VIVya_YF18)
+bot = Bot(token=TOKEN)
 
 def enviar_oferta(texto):
     bot.send_message(chat_id=CHAT_POST, text=texto)

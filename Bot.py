0e35d50f2f99597f1224608ddbfb9b4cc4bbd771
salvabjj <sirifile03@gmail.com>
2026-03-01 11:56:53 -0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,8 +1,18 @@
-from stores import amazon, mercadolivre, netshoes, zattini
-from utils.telegram import enviar_oferta, enviar_relatorio
+Arquivo de fluxo de trabalho para esta execução.
+.github/workflows/bot.yml em 3442c4d
+das lojas importam amazon, mercadolivre, netshoes, zattini
+de utils.telegram import enviar_oferta, enviar_relatorio
 from utils.history import ja_postado, adicionar_historico
 
-def executar():
+def executar() :
+ Verifique a falha na linha 5 em .github/workflows/bot.yml
+
+
+Ações do GitHub
+/ .github/workflows/bot.yml
+Arquivo de fluxo de trabalho inválido
+
+You have an error in your yaml syntax on line 5
 
     todas = []
     todas += mercadolivre.capturar()
@@ -13,28 +23,27 @@ def executar():
     total = 0
     por_loja = {}
 
-    for produto in todas:
-        if not ja_postado(produto["link"]):
+    para produto em todas :
+        se não ja_postado(produto["link"]) :
 
             mensagem = f"""
 🔥 {produto['titulo']}
 💰 R$ {produto['preco']}
 🏬 {produto['loja']}
 🔗 {produto['link']}
-"""
-
+" " "
             enviar_oferta(mensagem)
-            adicionar_historico(produto["link"])
+            adicionar_historico(produto[ " link"])
 
             total += 1
             loja = produto["loja"]
             por_loja[loja] = por_loja.get(loja, 0) + 1
 
-    relatorio = f"📊 RELATÓRIO\n\nTotal: {total}\n\n"
-    for loja, qtd in por_loja.items():
-        relatorio += f"{loja}: {qtd}\n"
+    relatorio = f"📊 RELATÓRIO\n\nTotal : {total}\n\n"
+    para loja, qtd em por_loja.items() :
+        relatorio += f"{loja} : {qtd}\n"
 
-    enviar_relatorio(relatorio)
+    enviar_relatório(relatório)
 
-if __name__ == "__main__":
+se __name__ == "__main__" :
     executar()

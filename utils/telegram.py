81e4d04be179a67c009bd852f25d854bde749217
salvabjj <sirifile03@gmail.com>
2026-03-01 12:11:04 -0300

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -1,9 +1,9 @@
 import os
 from telegram import Bot
 
-TOKEN = os.getenv("8056783817:AAHPYbLVK8tt7uYnVIjPg77J3VIVya_YF18")
-CHAT_POST = os.getenv("-1003858741896")
-CHAT_REPORT = os.getenv("8309912019")
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_POST = os.getenv("CHAT_ID_POST")
+CHAT_REPORT = os.getenv("CHAT_ID_REPORT")
 
 bot = Bot(token=TOKEN)
 
